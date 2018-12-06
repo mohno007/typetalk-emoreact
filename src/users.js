a005c1d43cdfdbcc8d564d336824dee36d869424
Motohiro OHNO <m-ohno@atware.co.jp>
2018-12-06 17:09:09 +0900

    ユーザ関連のテストを記述

--- src/users.js
+++ src/users.js
@@ -1,9 +1,15 @@
+/**
+ * ユーザ
+ */
 export class User {
   constructor(name) {
     Object.assign(this, { name });
   }
 }
 
+/**
+ * ユーザの集合
+ */
 export class Users {
   constructor(users) {
     Object.assign(this, { users });
--- test/users.test.js
+++ test/users.test.js
@@ -1,10 +1,30 @@
-import { User } from '../src/users.js';
+import { User, Users } from '../src/users.js';
 import Assert from 'assert';
 
 describe('User', function() {
-  it('should have name', function() {
-    const user = new User('name');
+  it('should have `name`', function() {
+    const user = new User('山田太郎');
 
-    Assert.equal(user.name, 'name');
+    Assert.equal(user.name, '山田太郎');
+  });
+});
+
+describe('Users', function() {
+  describe('#length', function() {
+    it('should return correct length of users', function() {
+      const users = new Users([
+        new User('山田太郎'),
+        new User('山田花子'),
+      ]);
+
+      Assert.equal(users.length(), 2);
+    });
+
+    it('should return 0 for empty users', function() {
+      const users = new Users([]);
+
+      Assert.equal(users.length(), 0);
+    });
+
   });
 });
